2ca6b37951c9451b7ee6cc04bb8ee916863ad2e7
Rochelle Gorbea <Rochelle.gorbea@outlook.com>
2019-02-11 13:02:44 -0800

    Added finding minimum solution

---
+++ Minimums/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimums
+{
+    public class Program
+    {
+        public static int minVal(int[] a)
+        {
+   
+            int smallest = a[0];
+
+            foreach (int i in a)
+            {
+                if (i < smallest)
+                {
+                   smallest = i;
+                }
+                
+            }
+            return smallest;
+        }
+        static void Main(string[] args)
+        {
+            int[] num = { 3, 1, 7, 8, 345 };
+            Console.WriteLine(minVal(num));
+        }
+    }
+}
---
+++ MinimumsTests/ProgramTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Minimums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimums.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void minValTest()
+        {
+            int[] a = { 324, 123, 2 };
+            Assert.AreEqual(2, Program.minVal(a));
+        }
+    }
+}
